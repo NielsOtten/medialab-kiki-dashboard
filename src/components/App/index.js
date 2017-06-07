ab663422310cf9fef8964baaa2be2e299c269f86
Niels <niels@burst-digital.com>
2017-06-07 09:21:57 +0200

    Add charts

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,5 +1,6 @@
 
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import IndexLink from 'react-router/lib/Link';
 import styles from './styles.scss';
 
