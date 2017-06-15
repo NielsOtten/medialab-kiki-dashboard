0f4af42c9349988cb57688ef2f4bb6372dd21736
Niels <niels@burst-digital.com>
2017-06-15 21:17:44 +0200

    Hover + active filter fix sizing graphs add h2 above graph

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -10,25 +10,16 @@ class Home extends React.Component {
 
     this.state = {
       games: [],
-      graphWidth: null,
+      active: 'total',
     };
-
-    this.onResize = this.onResize.bind(this);
   }
 
   componentDidMount() {
-    window.addEventListener('resize', this.onResize);
     this.clickHandler('total');
   }
 
-  onResize() {
-    if (this.wrapper && this.wrapper !== null) {
-      const graphWidth = this.wrapper.clientWidth - 30;
-      this.setState({ graphWidth });
-    }
-  }
-
   clickHandler = (total) => {
+    this.setState({ active: total });
     fetch(`/games?date=${total}`)
       .then(response => response.json())
       .then(json => {
@@ -46,40 +37,39 @@ class Home extends React.Component {
   };
 
   render() {
-    const graphWidth = (this.state.graphWidth == null && this.wrapper) ? this.wrapper.clientWidth : this.state.graphWidth;
     return(
       <div className={styles.wrapper}>
         <ul className={styles.filterList}>
-          <li className={styles.filter} onClick={this.clickHandler.bind(this, 'total')}>Totaal</li>
-          <li className={styles.filter} onClick={this.clickHandler.bind(this, 'month')}>Per maand</li>
-          <li className={styles.filter} onClick={this.clickHandler.bind(this, 'week')}>Per week</li>
-          <li className={styles.filter} onClick={this.clickHandler.bind(this, 'day')}>Per dag</li>
-          <li className={styles.filter} onClick={this.clickHandler.bind(this, 'today')}>Vandaag</li>
+          <li className={styles.filter + ' ' + (this.state.active === 'total' ? styles.active : '')} onClick={this.clickHandler.bind(this, 'total')}>Totaal</li>
+          <li className={styles.filter + ' ' + (this.state.active === 'month' ? styles.active : '')} onClick={this.clickHandler.bind(this, 'month')}>Per maand</li>
+          <li className={styles.filter + ' ' + (this.state.active === 'week' ? styles.active : '')} onClick={this.clickHandler.bind(this, 'week')}>Per week</li>
+          <li className={styles.filter + ' ' + (this.state.active === 'day' ? styles.active : '')} onClick={this.clickHandler.bind(this, 'day')}>Per dag</li>
+          <li className={styles.filter + ' ' + (this.state.active === 'today' ? styles.active : '')} onClick={this.clickHandler.bind(this, 'today')}>Vandaag</li>
         </ul>
-        <div className={styles.graphs}>
+        <div ref={(w) => this.wrapper = w} className={styles.graphs}>
           <div className={styles.graph}>
-            <div ref={(w) => this.wrapper = w} className={styles.graphWrapper}>
-              <ResponsiveContainer width={graphWidth} height={300}>
+            <h2 className={styles.speeltijd}>Speeltijd</h2>
+            <div className={styles.graphWrapper}>
+              <ResponsiveContainer width={100+'%'} height={400}>
                 <LineChart className={styles.lineChart} data={this.state.games}
                            margin={{top: 5, right: 30, left: 20, bottom: 5}}>
                   <XAxis dataKey="name"/>
                   <YAxis/>
                   <Tooltip/>
-                  <Legend />
                   <Line type="monotone" dataKey="Speeltijd" stroke="#82ca9d" />
                 </LineChart>
               </ResponsiveContainer>
             </div>
           </div>
           <div className={styles.graph}>
+            <h2 className={styles.munten}>Munten</h2>
             <div className={styles.graphWrapper}>
-              <ResponsiveContainer width={graphWidth} height={300}>
-                <LineChart className={styles.lineChart} width={400} height={300} data={this.state.games}
+              <ResponsiveContainer width={100+'%'} height={400}>
+                <LineChart className={styles.lineChart} data={this.state.games}
                            margin={{top: 5, right: 30, left: 20, bottom: 5}}>
                   <XAxis dataKey="name"/>
                   <YAxis/>
                   <Tooltip/>
-                  <Legend />
                   <Line type="monotone" dataKey="Munten" stroke="#82ca9d" />
                 </LineChart>
               </ResponsiveContainer>
