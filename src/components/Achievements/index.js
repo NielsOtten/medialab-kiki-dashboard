50fc3f5b6ab4b1bbd1195aec9258c567155dccc2
Niels <niels@burst-digital.com>
2017-06-15 14:23:12 +0200

    Translations for adding achievement

--- src/components/Achievements/index.js
+++ src/components/Achievements/index.js
@@ -83,13 +83,13 @@ class Achievements extends Component {
         <ul className={styles.achievements}>
           <li className={styles.achievement} onClick={this.clickHandler}>
             <svg className={styles.svg} version="1.1" id="Capa_1" x="0px" y="0px" viewBox="0 0 491.86 491.86"><g><g><path d="M465.167,211.614H280.245V26.691c0-8.424-11.439-26.69-34.316-26.69s-34.316,18.267-34.316,26.69v184.924H26.69 C18.267,211.614,0,223.053,0,245.929s18.267,34.316,26.69,34.316h184.924v184.924c0,8.422,11.438,26.69,34.316,26.69 s34.316-18.268,34.316-26.69V280.245H465.17c8.422,0,26.69-11.438,26.69-34.316S473.59,211.614,465.167,211.614z"></path></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g></svg>
-            <h2>Achievement toevoegen</h2>
+            <h2>Doel toevoegen</h2>
           </li>
           { this.state.achievements }
         </ul>
         <div className={styles.modal} style={{display: this.state.open ? 'block' : 'none'}} onClick={this.clickHandler}/>
         <div className={styles.addAchievement} style={{display: this.state.open ? 'block' : 'none'}}>
-          <h2>Achievement Toevoegen</h2>
+          <h2>Doel Toevoegen</h2>
           <form onSubmit={this.submitHandler}>
             <p>Doel</p>
             <div className={styles.radios}>
@@ -102,20 +102,8 @@ class Achievements extends Component {
                 <label htmlFor='munten'>Munten</label>
               </div>
               <div className={styles.radio}>
-                <input type='radio' onClick={this.radioClickHandler} name='achievement' value='Behaalde Achievements' id='achievement'/>
-                <label htmlFor='achievement'>Behaalde Achievements</label>
-              </div>
-              <div className={styles.radio}>
                 <input type='radio' onClick={this.radioClickHandler} name='achievement' value='Jumps' id='jumps'/>
-                <label htmlFor='jumps'>Jumps</label>
-              </div>
-              <div className={styles.radio}>
-                <input type='radio' onClick={this.radioClickHandler} name='achievement' value='Afstand in KM' id='distance'/>
-                <label htmlFor='distance'>Afstand in KM</label>
-              </div>
-              <div className={styles.radio}>
-                <input type='radio' onClick={this.radioClickHandler} name='achievement' value='Spierspanning' id='spanning'/>
-                <label htmlFor='spanning'>Spierspanning</label>
+                <label htmlFor='jumps'>Sprongen</label>
               </div>
             </div>
             <p>Waarde</p>
