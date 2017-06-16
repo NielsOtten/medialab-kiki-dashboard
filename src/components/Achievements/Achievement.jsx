58a8cb8613ba3d50aabf2ba3b459050cae714920
Niels <niels@burst-digital.com>
2017-06-16 08:52:46 +0200

    Add styling for delete button on achievement

--- src/components/Achievements/Achievement.jsx
+++ src/components/Achievements/Achievement.jsx
@@ -1,27 +1,39 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import styles from './styles.scss';
+import SVG from '../../images/SVG.jsx';
 
 class Achievement extends Component {
+  onDelete = (e) => {
+
+  };
+
   render() {
     const done = this.props.progress >= this.props.value ? styles.done : styles.notDone;
 
-    return <li className={styles.achievement}>
-      <img src="" alt=""/>
-      <h2 className={styles.title}>{this.props.title}</h2>
-      <span className={styles.gofor}>{`${this.props.value} ${this.props.target}`}</span>
-      <span className={[styles.progress, done].join(' ')}>{this.props.progress >= this.props.value ? 'Behaald' : 'Nog niet behaald'}</span>
-      <div className={[styles.toDo, done].join(' ')}>
-        <div className={styles.reached}>
-          <p className={styles.title}>Behaald</p>
-          <p className={styles.target}>{this.props.progress}</p>
+    return (
+      <li className={styles.achievement}>
+        <span className={styles.delete}>
+          <button onClick={this.onDelete}>
+            <SVG src='plus'/>
+          </button>
+        </span>
+        <img src="" alt=""/>
+        <h2 className={styles.title}>{this.props.title}</h2>
+        <span className={styles.gofor}>{`${this.props.value} ${this.props.target}`}</span>
+        <span className={[styles.progress, done].join(' ')}>{this.props.progress >= this.props.value ? 'Behaald' : 'Nog niet behaald'}</span>
+        <div className={[styles.toDo, done].join(' ')}>
+          <div className={styles.reached}>
+            <p className={styles.title}>Behaald</p>
+            <p className={styles.target}>{this.props.progress}</p>
+          </div>
+          <div className={styles.left}>
+            <p className={styles.title}>Te gaan</p>
+            <p className={styles.target}>{this.props.value - this.props.progress}</p>
+          </div>
         </div>
-        <div className={styles.left}>
-          <p className={styles.title}>Te gaan</p>
-          <p className={styles.target}>{this.props.value - this.props.progress}</p>
-        </div>
-      </div>
-    </li>;
+      </li>
+    );
   }
 }
 
